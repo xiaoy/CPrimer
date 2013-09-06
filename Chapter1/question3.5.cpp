8732f524d1639c716140ef7a6ec763085b2271a3
lfwu <zgwulongfei@gmail.com>
2013-09-06 18:53:01 +0800

    delete unused files

--- Chapter1/question3.5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string line;
-    while(getline(cin, line))
-    cout << line << endl;
-    return 0;
-}
--- Chapter1/question3.5b.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string word;
-    while(cin >> word)
-    cout << word << endl;
-    return 0;
-}
