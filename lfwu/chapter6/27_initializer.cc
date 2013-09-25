a3f4cab2aff5017f0e91d09725248cd3b945e392
lfwu <zgwulongfei@gmail.com>
2013-09-25 08:39:30 +0800

    add chapter6

---
+++ lfwu/chapter6/27_initializer.cc
@@ -0,0 +1,31 @@
+// -------------------------------------------------------------------
+// Author: lfwu
+// Date: September-24-2013
+// Note:
+// Email:zgwulongfei@gmail.com
+// Blog:http://blog.csdn.net/hackmind
+// -------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int Sum(initializer_list<int> list) {
+    int sum = 0;
+    for(auto& it : list) {
+        sum += it;
+    }
+    return sum;
+}
+
+void Log(vector<string> str_list) {
+    for(auto& it : str_list) {
+        cout<<it<<"\t";
+    }
+    cout<<endl;
+}
+int
+main(int argc, char** argv){
+    cout<<Sum({1, 2, 3 , 4})<<endl;
+    Log({"hello", "world"});
+}
---
+++ lfwu/chapter6/33_PrintVec.cc
@@ -0,0 +1,24 @@
+// -------------------------------------------------------------------
+// Author: lfwu
+// Date: September-24-2013
+// Note:
+// Email:zgwulongfei@gmail.com
+// Blog:http://blog.csdn.net/hackmind
+// -------------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void PrintVec(vector<string>& str_list, vector<string>::iterator it) {
+    if(it != str_list.end()) {
+        cout<<*it<<endl;
+        it++;
+        PrintVec(str_list, it);
+    }
+}
+int
+main(int argc, char** argv){
+    vector<string> str_list = {"hello", "world"};
+    PrintVec(str_list, begin(str_list));
+}
