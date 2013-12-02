52c33b119697ac92364aae4e306ad0d534f3b30c
lfwu <zgwulongfei@gmail.com>
2013-12-02 08:29:34 +0800

    add new chapter

---
+++ lfwu/chapter8/1readstr.cc
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------
+// Author: lfwu
+// Date: December-02-2013
+// Note:
+// Email:zgwulongfei@gmail.com
+// Blog:http://blog.csdn.net/hackmind
+// -------------------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
+
+istream& readstr(istream& in) {
+    string str;
+    while(in>>str)
+        cout<<str<<endl;
+    in.clear();
+    return in;
+}
+int
+main(int argc, char** argv){
+    readstr(cin);
+}
