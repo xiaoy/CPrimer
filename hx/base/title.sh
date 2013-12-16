9f92befe0fcf1c6cf5f6a016fefae66cd582b379
dandelions <dandelions_007@126.com>
2013-12-16 21:38:21 +0800

    add basic oper shell-script

---
+++ hx/base/title.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+echo "// --------------------------------------------"
+echo "// Author: hx"
+echo "// Date:" `date +"%B-%d-%Y"`
+echo "// Note:"
+echo "// ---------------------------------------------"
+echo "#include <iostream>"
+echo ""
+echo "int main(){"
+echo "	using namespace std;"
+echo ""
+echo "	return 0;"
+echo "}"
---
+++ hx/chapter3/exercise/newfile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+if [ $# -ne 1 ]
+then
+    echo "wrong args"
+    exit 0
+fi
+
+if [ ! -f "$1" ]
+then
+    touch $1
+    . ../../base/title.sh>>$1
+fi
+
