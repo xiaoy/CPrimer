a88290ac26a0ca88011f8dc0ee6ebe93d350dc4c
dandelions <dandelions_007@126.com>
2013-12-19 20:45:56 +0800

    finish chapter4

--- hx/chapter4/main/strtype1.cpp
+++ hx/chapter4/main/strtype1.cpp
@@ -9,7 +9,21 @@
 int main(){
 	using namespace std;
 
-	
+	char char1[20];
+	char char2[20]="jaguar";
+
+	string str1;
+	string str2="panter";
+
+	cout<<"Enter char1:";
+	cin>>char1;
+	cout<<"Enter str1 ";
+	cin>>str1;
+	cout<<"char1:"<<char1<<";char2:"<<char2<<endl;
+	cout<<"str1:"<<str1<<";str2:"<<str2<<endl;
+
+	cout<<"char2[2] is "<<char2[2];
+	cout<<"\nstr2[2] is " <<str2[2]<<endl;
 
 	return 0;
 }
---
+++ hx/chapter5/main/forloop.cpp
@@ -0,0 +1,17 @@
+// --------------------------------------------
+// Author: hx
+// Date: 十二月-19-2013
+// Note:
+// ---------------------------------------------
+#include <iostream>
+
+int main(){
+	using namespace std;
+
+	int i;
+	for(i=0;i<5;i++)
+		cout<<"it's "<<i<<" times input"<<endl;
+	cout<<"it's end"<<endl;
+
+	return 0;
+}
