74e3bbe3642f3100311415fedbf3f0b29fdef051
lfwu <zgwulongfei@gmail.com>
2013-12-22 22:36:44 +0800

    submit new exercise

--- lfwu/chapter9/utility.h
+++ lfwu/chapter9/utility.h
@@ -7,10 +7,34 @@
 // -------------------------------------------------------------------
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 template <class T>
 void PrintContainer(T& con, char det= ' ') {
     for(auto it = con.cbegin(); it != con.cend(); ++it)
         cout << *it << det;
+}
+
+template<class T>
+void CreateSeq(T& con, int start, int end) {
+    while(start <= end) {
+        con.push_back(start);
+        start += 1;
+    }
+}
+
+static int seed = time(nullptr);
+int GetRandNum(int min, int max) {
+    srand(seed);
+    seed = rand();
+    return (min + seed)%(max - min + 1);
+}
+
+template <class T>
+void CreateRandSeq(T& con, size_t len, int min, int max) {
+    size_t i = 0;
+    while(i++ < len) {
+        con.push_back(GetRandNum(min, max));
+    }
 }
