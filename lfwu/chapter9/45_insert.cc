2b429dd08c02a8e1410e6ad3ef02d42bc253a709
lfwu <zgwulongfei@gmail.com>
2013-12-23 08:27:19 +0800

    add new exercise

---
+++ lfwu/chapter9/45_insert.cc
@@ -0,0 +1,24 @@
+// -------------------------------------------------------------------
+// Author: lfwu
+// Date: December-23-2013
+// Note:
+// Email:zgwulongfei@gmail.com
+// Blog:http://blog.csdn.net/hackmind
+// -------------------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
+
+int
+main(int argc, char** argv){
+    string name = "lfwu";
+    cout << name << endl;
+
+    string prefix = "Mr.";
+    string suffix = " III";
+
+    name.insert(0, prefix);
+    name.append(suffix);
+
+    cout << name << endl;
+}
