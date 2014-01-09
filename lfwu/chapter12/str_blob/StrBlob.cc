9ecdf0724630fe1b70bfdf5edec4a415d445399f
lfwu <zgwulongfei@gmail.com>
2014-01-09 20:55:44 +0800

    update code

--- lfwu/chapter12/str_blob/StrBlob.cc
+++ lfwu/chapter12/str_blob/StrBlob.cc
@@ -22,36 +22,43 @@ StrBlob::StrBlob(initializer_list<string> li):
 StrBlob::~StrBlob() {
 }
 
-void StrBlob::PushBack(const std::string& str) {
+void
+StrBlob::PushBack(const std::string& str) {
     data_->push_back(str);
 }
 
-void StrBlob::Pop() {
+void
+StrBlob::Pop() {
     Check(0, "pop on empty StrBlob");
     data_->pop_back();
 }
 
-const std::string& StrBlob::Front() {
+const std::string&
+StrBlob::Front() {
     Check(0, "front on empty StrBlob");
     return data_->front();
 }
 
-const std::string& StrBlob::Back() {
+const std::string&
+StrBlob::Back() {
     Check(0, "back on empty StrBlob");
    return data_->back();
 }
 
-const std::string& StrBlob::Front() const {
+const std::string&
+StrBlob::Front() const {
     Check(0, "front on empty StrBlob");
     return data_->front();
 }
 
-const std::string& StrBlob::Back() const {
+const std::string&
+StrBlob::Back() const {
    Check(0, "back on empty StrBlob");
    return data_->back();
 }
 
-void StrBlob::Check(size_type i, const string& str) const {
+void
+StrBlob::Check(size_type i, const string& str) const {
     if(i >= data_->size())
         throw std::out_of_range(str);
 }
